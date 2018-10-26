b9278d4fea02e3eeefa772ed03ba833a1f085d70
Ricardo Castro <ricardocastrosis@gmail.com>
2018-10-26 18:43:15 -0300

    novas paginas

---
+++ Projeto/admin/pages/painel-content.php
@@ -0,0 +1,37 @@
+
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Breadcrumbs-->
+            <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="#">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Overview</li>
+            </ol>
+
+            <?php include_once "pages/cards.php"; ?>
+
+            <?php include_once "pages/chart.php"; ?>
+
+        
+            <?php include_once "pages/tabela.php"; ?>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <!-- Sticky Footer -->
+        <footer class="sticky-footer">
+            <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright © Your Website 2018</span>
+            </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.content-wrapper -->
+        
+
+    
